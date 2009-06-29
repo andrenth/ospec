c4b613f800db378c921bda2a49ecdbe696504489
Andre Nathan <andre@digirati.com.br>
2009-06-29 15:25:36 -0300

    Add parenthesis for clarity when reporting failures.

--- report.ml
+++ report.ml
@@ -4,9 +4,9 @@ open Printf
 let report_failure failure =
   let expected =
     match Spec.failure_expected_result failure with
-    | Some e -> sprintf " %s" e
+    | Some e -> sprintf " (%s)" e
     | None -> "" in
-  printf "%d) Expected `%s %s%s' to return %s\n"
+  printf "%d) Expected `%s (%s)%s' to return %s\n"
          (Spec.failure_id failure) (Spec.failure_operation failure)
          (Spec.failure_actual_result failure) expected
          (if Spec.positive_failure failure then "true" else "false")
